07a820e31e51a338a30b0674c75cdda57e0b10ad
Hiroyuki Kuromiya <15026387+kromiii@users.noreply.github.com>
2024-10-19 20:27:32 +0900

    一旦整理

---
+++ superset/superset_config.py
@@ -0,0 +1,9 @@
+FEATURE_FLAGS = {
+    "ENABLE_TEMPLATE_PROCESSING": True,
+}
+
+ENABLE_PROXY_FIX = True
+SECRET_KEY = "YOUR_OWN_RANDOM_GENERATED_STRING"
+
+# Database configuration
+SQLALCHEMY_DATABASE_URI = 'postgresql://superset:superset@superset-postgres:5432/superset'
